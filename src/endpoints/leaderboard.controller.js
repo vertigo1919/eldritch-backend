734042bb60bb619efe96bcab067c85044c1177dd
yerayafonso <yafonso02@gmail.com>
2026-03-19 00:34:14 +0000

    add console.log to leaderboard controller

--- src/endpoints/leaderboard.controller.js
+++ src/endpoints/leaderboard.controller.js
@@ -2,6 +2,7 @@ import { getLeaderboardService } from './leaderboard.service.js';
 
 export function getLeaderboard(req, res, next) {
   const { offset } = req.query;
+  console.log('query', offset);
   getLeaderboardService(offset)
     .then((users) => {
       res.status(200).send(users);
